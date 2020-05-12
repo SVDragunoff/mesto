1c3fb85fc3ea2fb173a37990663c990d2187e41d
SVDragunov <asdf113322@yandex.ru>
2020-05-12 03:11:08 +0300

    ready save

--- scrypt.js
+++ scrypt.js
@@ -1,18 +1,21 @@
-let editProfile = document.querySelector('.profile__info-button');
-let closeProfile = document.querySelector('.popup__button-close')
+const editProfile = document.querySelector('.profile__info-button');
+const closeProfile = document.querySelector('.popup__button-close');
 
-function openProfile () {
+function openPopup () {
     document.querySelector('.popup').style.display = "flex";
 }
-function closePr () {
+function closePopup () {
     document.querySelector('.popup').style.display = "none";
 }
-editProfile.addEventListener('click', openProfile);
-closeProfile.addEventListener('click', closePr);
+editProfile.addEventListener('click', openPopup);
+closeProfile.addEventListener('click', closePopup);
+
+const profileName = document.querySelector('.profile__info-title');
+const profileJob = document.querySelector('.profile__info-subtitle');
 
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__conteiner');
+let formElement = document.querySelector('.popup');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -22,15 +25,19 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
                                                 
     // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup__inputs');
-    let jobInput = document.querySelector('.popup__button-title');
+    let nameInput = document.querySelector('.popup__input_name');
+    let jobInput = document.querySelector('.popup__input_job');
     // Получите значение полей из свойства value
-
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     // Выберите элементы, куда должны быть вставлены значения полей
-    nameInput.textContent = document.querySelector('.profile__info-title');
+    
     // Вставьте новые значения с помощью textContent
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+//formElement.addEventListener('submit', formSubmitHandler);
+const saveButton = document.querySelector('.popup__button-save');
+    saveButton.addEventListener('click',formSubmitHandler );
