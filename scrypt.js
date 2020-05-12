2e1b8a320acbc4d3d4dd8ddf27601721aa55053f
SVDragunov <asdf113322@yandex.ru>
2020-05-12 23:13:30 +0300

    ready

--- scrypt.js
+++ scrypt.js
@@ -14,30 +14,20 @@ const profileName = document.querySelector('.profile__info-title');
 const profileJob = document.querySelector('.profile__info-subtitle');
 
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+const formElement = document.querySelector('.popup');
+
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-                                                
-    // Находим поля формы в DOM
+    evt.preventDefault(); 
     let nameInput = document.querySelector('.popup__input_name');
     let jobInput = document.querySelector('.popup__input_job');
-    // Получите значение полей из свойства value
+  
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-    
-    // Вставьте новые значения с помощью textContent
+
     closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-//formElement.addEventListener('submit', formSubmitHandler);
 const saveButton = document.querySelector('.popup__button-save');
     saveButton.addEventListener('click',formSubmitHandler );
