56968db127fb68b9e1f399f87169872c0471ffb6
SVDragunov <asdf113322@yandex.ru>
2020-05-14 01:26:56 +0300

    2 send

---
+++ JS/scrypt.js
@@ -0,0 +1,40 @@
+const editProfile = document.querySelector('.profile__info-button');
+const closeProfile = document.querySelector('.popup__button-close');
+const formElement = document.querySelector('.popup');
+const saveButton = document.querySelector('.popup__button-save');
+
+//нужно разобраться, как объединить функции открытия и закрытия PopupА
+function openPopup () {
+    formElement.classList.add("popup_open");
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+function closePopup () {
+    formElement.classList.remove("popup_open");
+}
+
+
+const profileName = document.querySelector('.profile__info-title');
+const profileJob = document.querySelector('.profile__info-subtitle');
+
+
+
+
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    let nameInput = document.querySelector('.popup__inputs_name');
+    let jobInput = document.querySelector('.popup__inputs_job');
+  
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    closePopup();
+}
+
+editProfile.addEventListener('click', openPopup);
+closeProfile.addEventListener('click', closePopup);
+
+formElement.addEventListener('submit', formSubmitHandler);
+
