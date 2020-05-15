ee61e15dae58387ae2cbff4ca7a91e59cf1910c5
SVDragunov <asdf113322@yandex.ru>
2020-05-15 19:44:53 +0300

    final

--- JS/scrypt.js
+++ JS/scrypt.js
@@ -2,20 +2,23 @@ const editProfile = document.querySelector('.profile__info-button');
 const closeProfile = document.querySelector('.popup__button-close');
 const formElement = document.querySelector('.popup');
 const saveButton = document.querySelector('.popup__button-save');
+const profileName = document.querySelector('.profile__info-title');
+const profileJob = document.querySelector('.profile__info-subtitle');
+let nameInput = document.querySelector('.popup__inputs_name');
+let jobInput = document.querySelector('.popup__inputs_job');
 
 //нужно разобраться, как объединить функции открытия и закрытия PopupА
 function openPopup () {
-    formElement.classList.add("popup_open");
+    formElement.classList.add('popup_open');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 function closePopup () {
-    formElement.classList.remove("popup_open");
+    formElement.classList.remove('popup_open');
 }
 
 
-const profileName = document.querySelector('.profile__info-title');
-const profileJob = document.querySelector('.profile__info-subtitle');
+
 
 
 
@@ -24,8 +27,7 @@ const profileJob = document.querySelector('.profile__info-subtitle');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let nameInput = document.querySelector('.popup__inputs_name');
-    let jobInput = document.querySelector('.popup__inputs_job');
+
   
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
